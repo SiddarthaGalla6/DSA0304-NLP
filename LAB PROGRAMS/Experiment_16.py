a5bda6a221cbb0b465101f88308a3b8b176d03c3
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-19 11:44:44 +0530

    Update Experiment_16.py

--- LAB PROGRAMS/Experiment_16.py
+++ LAB PROGRAMS/Experiment_16.py
@@ -1,21 +1,16 @@
 import spacy
-
-# Install model once if needed:
-# python -m spacy download en_core_web_sm
-
 nlp = spacy.load("en_core_web_sm")
-
 text = input("Enter text: ")
 doc = nlp(text)
-
 print("Named Entities:")
 for ent in doc.ents:
     print(ent.text, "->", ent.label_)
 
-# Sample Input:
-# Apple is in California.
-#
-# Sample Output:
-# Named Entities:
-# Apple -> ORG
-# California -> GPE
+
+Input:
+Apple is in California.
+
+Output:
+Named Entities:
+Apple -> ORG
+California -> GPE
