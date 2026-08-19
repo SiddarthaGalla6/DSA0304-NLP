3070d00bd2900eb9f1f7d37ab7822457c2e2c000
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-19 11:44:08 +0530

    Fix formatting issues in Experiment_15.py

--- LAB PROGRAMS/Experiment_15.py
+++ LAB PROGRAMS/Experiment_15.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk import PCFG
 from nltk.parse import ViterbiParser
-
 grammar = PCFG.fromstring("""
 S -> NP VP [1.0]
 NP -> Det N [1.0]
@@ -10,10 +9,8 @@ Det -> 'the' [0.5] | 'a' [0.5]
 N -> 'boy' [0.5] | 'girl' [0.5]
 V -> 'sees' [0.5] | 'likes' [0.5]
 """)
-
 parser = ViterbiParser(grammar)
 sentence = input("Enter a sentence: ").lower().split()
-
 try:
     tree = next(parser.parse(sentence))
     print("Most Probable Parse:")
@@ -21,7 +18,8 @@ try:
 except StopIteration:
     print("Sentence cannot be parsed.")
 
-# Sample Output:
-# Enter a sentence: the boy sees a girl
-# Most Probable Parse:
-# (S (NP (Det the) (N boy)) (VP (V sees) (NP (Det a) (N girl))))
+
+Output:
+Enter a sentence: the boy sees a girl
+Most Probable Parse:
+(S (NP (Det the) (N boy)) (VP (V sees) (NP (Det a) (N girl))))
