695f0bfe6f99c730a85c3334864cc678addb9de2
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-19 11:42:11 +0530

    Update Experiment_12.py

--- LAB PROGRAMS/Experiment_12.py
+++ LAB PROGRAMS/Experiment_12.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk import CFG
 from nltk.parse import EarleyChartParser
-
 grammar = CFG.fromstring("""
 S -> NP VP
 NP -> Det N
@@ -10,19 +9,17 @@ Det -> 'the' | 'a'
 N -> 'boy' | 'girl'
 V -> 'sees' | 'likes'
 """)
-
 parser = EarleyChartParser(grammar)
 sentence = input("Enter a sentence: ").lower().split()
-
 trees = list(parser.parse(sentence))
-
 if trees:
     print("Sentence Accepted")
     print(trees[0])
 else:
     print("Sentence Rejected")
 
-# Sample Output:
-# Enter a sentence: the boy sees a girl
-# Sentence Accepted
-# (S (NP (Det the) (N boy)) (VP (V sees) (NP (Det a) (N girl))))
+
+Output:
+Enter a sentence: the boy sees a girl
+Sentence Accepted
+(S (NP (Det the) (N boy)) (VP (V sees) (NP (Det a) (N girl))))
