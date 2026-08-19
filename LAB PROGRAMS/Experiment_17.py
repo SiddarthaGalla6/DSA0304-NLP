f8ebfe3470cc688ac48241e102c59f0775346284
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-19 11:45:19 +0530

    Update Experiment_17.py

--- LAB PROGRAMS/Experiment_17.py
+++ LAB PROGRAMS/Experiment_17.py
@@ -1,11 +1,8 @@
 from nltk.corpus import wordnet as wn
 import nltk
-
 nltk.download("wordnet")
-
 word = input("Enter a word: ")
 synsets = wn.synsets(word)
-
 if synsets:
     for syn in synsets[:3]:
         print("Synset:", syn.name())
@@ -13,9 +10,10 @@ if synsets:
 else:
     print("No synsets found.")
 
-# Sample Input:
-# Enter a word: bank
-#
-# Sample Output:
-# Synset: bank.n.01
-# Definition: sloping land (especially the slope beside a body of water)
+
+Input:
+Enter a word: bank
+
+Sample Output:
+Synset: bank.n.01
+Definition: sloping land (especially the slope beside a body of water)
